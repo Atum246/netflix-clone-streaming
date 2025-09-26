65ae1e7dddf8a6467a58ef3c09d1fddbdbe495d9
Atum246 <akmarketing2045@gmail.com>
2025-09-26 11:15:50 +0100

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['image.tmdb.org', 'rb.gy', 'raw.githubusercontent.com'],
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
